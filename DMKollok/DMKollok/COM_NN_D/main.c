88078ca2833211c19c5ee6407769ff1d3b4a2caa
Pupsik <ThePupsick@yandex.ru>
2017-03-17 01:17:33 +0300

    add check NullPointer

--- DMKollok/DMKollok/COM_NN_D/main.c
+++ DMKollok/DMKollok/COM_NN_D/main.c
@@ -4,10 +4,21 @@
 
 // Функция для тестирования моей функции
 
+int compNumbers(NUMBN fNumb, NUMBN sNumb);
 
 int main(void)
     {
-        
+        NUMBN a,b;
+        a.n = 5;
+        b.n = 5;
+        a.A = (int*) malloc(a.n*sizeof(int));
+        b.A = (int*) malloc(b.n*sizeof(int));
+        for(int i =0; i < 5; ++i)
+            {
+                scanf("%d",(a.A+i));
+                scanf("%d",(b.A+i));
+            }
+        printf("%d\n",compNumbers(a, b));
         
         // Позже здесь будет код!
     
@@ -15,35 +26,42 @@ int main(void)
     }
 
 /*
+
  compNumbers
  
  Функция сравнивает два целых числа, и возвращает: 2, если первое больше второго,
- 0, если они равны, иначе 1
+ 0, если они равны, иначе 1, если ошибка, то выводит -1
  
  Параметры:
  1) fNumb - Первое число
  2) sNumb - Второе число
  
  Функция возвращает результат типа - int
- */
+ 
+*/
 
 
 int compNumbers(NUMBN fNumb, NUMBN sNumb)
     {
         int answer = 0;
-        if(fNumb.n == sNumb.n)
+        if(fNumb.A && sNumb.A)
             {
-                int len = fNumb.n,
-                    fl = 0;
-                for(int i = 0; (i < len) && !fl; ++i )
+                if(fNumb.n == sNumb.n)
                     {
-                        fl = i < len && *(fNumb.A+i) == *(sNumb.A+i);
-                        if((!fl) && (i != len))
-                            answer = *(fNumb.A+i) > *(sNumb.A+i)? 2: 1;
+                        int len = fNumb.n,
+                            fl = 0;
+                        for(int i = 0; (i < len) && !fl; ++i )
+                            {
+                                fl = i < len && *(fNumb.A+i) == *(sNumb.A+i);
+                                if((!fl) && (i != len))
+                                    answer = *(fNumb.A+i) > *(sNumb.A+i)? 2: 1;
+                            }
                     }
+                else
+                    answer = *(fNumb.A) > *(sNumb.A)? 2: 1;
             }
         else
-            answer = *(fNumb.A) > *(sNumb.A)? 2: 1;
+            answer = -1;
         
         return answer;
     }
