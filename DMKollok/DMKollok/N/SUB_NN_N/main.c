0555ba72e8bcf99b9b68795fe76ddfe11a432692
Pupsik <ThePupsick@yandex.ru>
2017-03-20 12:16:01 +0300

    begin work

--- DMKollok/DMKollok/N/SUB_NN_N/main.c
+++ DMKollok/DMKollok/N/SUB_NN_N/main.c
@@ -1,15 +1,9 @@
-//
-//  main.c
-//  SUB_NN_N
-//
-//  Created by Виталя on 20.03.17.
-//  Copyright © 2017 Dreams of Pines. All rights reserved.
-//
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(void) {
+    
     printf("Hello, World!\n");
     return 0;
+
 }
