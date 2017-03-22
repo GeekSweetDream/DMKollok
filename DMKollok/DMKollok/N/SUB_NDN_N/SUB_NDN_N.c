328d209758f73e76b510de62ee203d07d65e9e88
Pupsik <ThePupsick@yandex.ru>
2017-03-22 15:32:29 +0300

    add final SUB_NDN_N

--- DMKollok/DMKollok/N/SUB_NDN_N/SUB_NDN_N.c
+++ DMKollok/DMKollok/N/SUB_NDN_N/SUB_NDN_N.c
@@ -6,34 +6,23 @@
 #include "COM_NN_D/COM_NN_D.h"
 #include "SUB_NN_N/bfunc/bfunc.h"
 
-NUMBN differentNumbMultK (NUMBN fNumb, NUMBN sNumb, int k);
-
-int main(void) {
-    
-    NUMBN a, b,c;
-    a.n = 2;
-    b.n = 3;
-    a.A = (int*) malloc(a.n*sizeof(int));
-    b.A = (int*) malloc(b.n*sizeof(int));
-    
-    for (int i = 0; i < a.n; ++i) {
-        scanf("%d", a.A+i);
-    }
-    
-    for (int i = 0; i < b.n; ++i) {
-        scanf("%d", b.A+i);
-    }
-    
-    c = differentNumbMultK(a,b, 4);
-
-    for (int i = 0; i < c.n; ++i) {
-        printf("%d", *(c.A+i));
-    }
-
-    
-    return 0;
-}
-
+/*
+ 
+ Написал: Пискунов Виталий
+ Группа: 6308
+ 
+ differentNumbMultK
+ 
+ Функция вычитание из одного натурального другое умноженное на цифру
+ 
+ Параметры:
+ 1) numbF - Первое число
+ 2) numbS - Второе число
+ 3) k - цифра, на которую будет умножаться число
+ 
+ Функция возращает тип - NUMBN
+ 
+ */
 
 NUMBN differentNumbMultK (NUMBN fNumb, NUMBN sNumb, int k)
     {
