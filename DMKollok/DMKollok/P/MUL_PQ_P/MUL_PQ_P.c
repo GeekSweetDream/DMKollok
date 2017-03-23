469d2e70f32fb44f2db83f5a3e9a9ccfc43758fb
sawkamishanov <alexander.mishanov@gmail.com>
2017-03-23 22:26:35 +0300

    add MUL_PQ_P

---
+++ DMKollok/DMKollok/P/MUL_PQ_P/MUL_PQ_P.c
@@ -0,0 +1,40 @@
+﻿/*
+
+ multiplicationNumbPQ
+ Функция для умножения многочлена на рациональное число. Возвращает: B - результат умножения 
+ многочлена на рациональное число
+ Параметры:
+ 1) A - многочлен
+ 2) Q - рациональное число
+ Функция возвращает результат типа - NUMBP
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "numbP.h"
+#include "MUL_PQ_P.h"
+#include "MUL_QQ_Q/MUL_QQ_Q.h"
+
+NUMBP multiplicationNumbPQ(NUMBP A, NUMBQ Q)
+{
+	NUMBP B;
+
+	if (A.C)
+	{
+		B.m = A.m;
+		B.C = (NUMBQ*)malloc(B.m * sizeof(NUMBQ));
+		for (int i = 0;i < B.m;++i)
+			B.C[i] = multiplicationNumbQ(A.C[i], Q);
+	}
+	else
+		printf("Многочлена не существует!\n");
+
+	return B;
+}
+
+
+
+
+
+
