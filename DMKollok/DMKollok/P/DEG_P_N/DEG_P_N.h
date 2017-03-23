51b091efa23432a0231f62bc050cea9f4335e604
sawkamishanov <alexander.mishanov@gmail.com>
2017-03-23 23:12:00 +0300

    DEG_P_N

---
+++ DMKollok/DMKollok/P/DEG_P_N/DEG_P_N.h
@@ -0,0 +1,24 @@
+﻿#ifndef _DEG_P_N_H_
+#define _DEG_P_N_H_
+
+#include "numbP.h"
+
+/*
+
+ Автор: Мишанов Александр
+ Группа: 6308
+
+ getDegree
+
+ Функция получает степень многочлена
+
+ Параметры:
+ 1)A - многочлен
+
+ Функция возвращает - int
+
+*/
+
+int getDegree(NUMBP A);
+
+#endif
