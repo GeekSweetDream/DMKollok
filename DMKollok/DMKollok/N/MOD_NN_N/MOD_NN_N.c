ab7acff20821a75ebffcdd146cf4c214f0b97804
Pupsik <ThePupsick@yandex.ru>
2017-03-23 17:36:19 +0300

    Create lib mod

--- DMKollok/DMKollok/N/MOD_NN_N/MOD_NN_N.c
+++ DMKollok/DMKollok/N/MOD_NN_N/MOD_NN_N.c
@@ -4,34 +4,24 @@
 #include "DIV_NN_N/DIV_NN_N.h"
 #include "DIV_NN_N/SUB_NDN_N/SUB_NN_N/SUB_NN_N.h"
 #include "DIV_NN_N/MUL_NN_N/MUL_NN_N.h"
+#include "MOD_NN_N.h"
 
-NUMBN modNumbN(NUMBN a, NUMBN b);
-
-int main(int argc, const char * argv[]) {
-    NUMBN a,b,c;
-    
-    a.n = 3;
-    b.n = 3;
-    
-    a.A = (int*) malloc(a.n * sizeof(int));
-    b.A = (int*) malloc(b.n * sizeof(int));
-    
-    for (int i = 0 ; i<a.n; ++i) {
-        scanf("%d",a.A+i);
-    }
-    
-    for (int i = 0 ; i<b.n; ++i) {
-        scanf("%d",b.A+i);
-    }
-    
-    c = modNumbN(a, b);
-    
-    for (int i = 0 ; i<c.n; ++i) {
-        printf("%d",*(c.A+i));
-    }
-    
-    return 0;
-}
+/*
+ 
+ Написал: Пискунов Виталий
+ Группа: 6308
+ 
+ modNumbN
+ 
+ Функция выдает остаток от деления
+ 
+ Параметры:
+ 1) a - делимое
+ 2) b - делитель
+ 
+ Функция возращает тип - NUMBN
+ 
+ */
 
 
 NUMBN modNumbN(NUMBN a, NUMBN b)
