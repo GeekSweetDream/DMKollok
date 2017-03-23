1c78db2777d5cc221cc8e6e20069d35ef2df90e6
Pupsik <ThePupsick@yandex.ru>
2017-03-23 12:38:42 +0300

    add two TRANS

---
+++ DMKollok/DMKollok/Q/TRANS_Z_Q/TRANS_Z_Q.h
@@ -0,0 +1,18 @@
+#ifndef _TRANS_Z_Q_H_
+#define _TRANS_Z_Q_H_
+
+#include "numbQ.h"
+#include "numbZ.h"
+
+
+/*
+getNumbQ
+Функция пребразует целое в дробное
+Параметры:
+1) a - число
+Функция возвращает результат типа - NUMBQ
+*/
+
+NUMBQ getNumbQ(NUMBZ a);
+
+#endif
