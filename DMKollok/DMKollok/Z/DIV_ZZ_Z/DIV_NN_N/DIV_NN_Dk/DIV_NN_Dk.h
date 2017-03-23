2530265c097ca7114baeeb1af68621a79506c6d9
Pupsik <ThePupsick@yandex.ru>
2017-03-23 21:41:21 +0300

    add lib

---
+++ DMKollok/DMKollok/Z/DIV_ZZ_Z/DIV_NN_N/DIV_NN_Dk/DIV_NN_Dk.h
@@ -0,0 +1,24 @@
+#ifndef _DIV_NN_Dk_H_
+#define _DIV_NN_Dk_H_
+#include "numbN.h"
+
+/*
+ 
+ Написал: Пискунов Виталий
+ Группа: 6308
+ 
+ getFirstNumbDivident
+ 
+ Функция выдает первую цифру большего числа умн на 10^k
+ 
+ Параметры:
+ 1) fNumb - Первое число
+ 2) sNumb - Второе число
+ 
+ Функция возращает тип - NUMBN
+ 
+ */
+
+
+NUMBN getFirstNumbDivident(NUMBN fNumb,NUMBN sNumb);
+#endif
