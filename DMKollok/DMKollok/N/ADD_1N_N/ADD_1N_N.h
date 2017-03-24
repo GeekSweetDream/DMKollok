cc47c654fac34b9932ccc14a48d2525721f71cad
Pupsik <ThePupsick@yandex.ru>
2017-03-24 14:47:05 +0300

    Add ADD_1N_N

---
+++ DMKollok/DMKollok/N/ADD_1N_N/ADD_1N_N.h
@@ -0,0 +1,20 @@
+#ifndef _ADD_1N_N
+#define _ADD_1N_N
+
+#include "numbN.h"
+/*
+
+ AddOne
+ 
+ Функция для прибавления 1
+ 
+ Параметры
+ 1) NUMBN numb - Число, к которому будут прибавлять
+ 
+ 
+  Фукнция выводит тип - NUMBN
+ 
+ */
+
+NUMBN AddOne(NUMBN numb);
+#endif
