1a835baab6258d69eaf18d4adb49edf937d40b55
sawkamishanov <alexander.mishanov@gmail.com>
2017-03-24 14:18:12 +0300

    ADD_PP_P

---
+++ DMKollok/DMKollok/P/ADD_PP_P/ADD_PP_P.h
@@ -0,0 +1,21 @@
+﻿#ifndef _ADD_PP_P_H_
+#define _ADD_PP_P_H_
+#include "numbP.h"
+
+/*
+
+ addNumbP
+
+ Функция для сложения двух многочленов
+
+ Параметры:
+ 1) A - первый многочлен
+ 2) B - второй многочлен
+
+ Функция возвращает результат типа - NUMBP
+
+*/
+
+NUMBP addNumbP(NUMBP A, NUMBP B);
+
+#endif
---
+++ DMKollok/DMKollok/P/ADD_PP_P/ADD_QQ_Q/ADD_ZZ_Z/ABS_Z_N/ABS_Z_N.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "numbZ.h"
+#include "numbN.h"
+#include "ABS_Z_N.h"
+
+NUMBN absNumbZ(NUMBZ numb);
+
+/*
+ 
+ Написал: Пискунов Виталий
+ Группа: 6308
+ 
+ absNumbZ
+ 
+ Функция выдает абсолютное значение(натуральное) целого числа
+ 
+ Параметры:
+ 1) numb - Целое число
+ 
+ Функция возращает тип - NUMBN
+ 
+*/
+
+NUMBN absNumbZ(NUMBZ numb)
+    {
+        NUMBN answer;
+        
+        if(numb.A)
+            {
+                answer.n = numb.n;
+                answer.A = (int*) malloc(answer.n*sizeof(int));
+                for (int i = 0; i < numb.n; ++i)
+                    *(answer.A+i) = *(numb.A+i);
+            }
+        else
+            printf("\nОшибка, числа не существует!\n");
+        return answer;
+    }
+
