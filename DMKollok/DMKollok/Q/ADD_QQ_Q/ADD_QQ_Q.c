d74790a0ef699810deb342d8b3c52f6e12e25784
sawkamishanov <alexander.mishanov@gmail.com>
2017-03-24 05:29:33 +0300

    add ADD_QQ_Q, SUB_QQ_Q

---
+++ DMKollok/DMKollok/Q/ADD_QQ_Q/ADD_QQ_Q.c
@@ -0,0 +1,34 @@
+﻿/*
+
+ subtractNumbQ
+
+ Функция вычитает два рациональных числа
+ Параметры:
+ 1) A - первое число
+ 2) B - второе число
+
+ Функция возвращает результат типа - NUMBQ
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "numbQ.h"
+#include "ADD_QQ_Q.h"
+#include "TRANS_N_Z/TRANS_N_Z.h"
+#include "MUL_ZZ_Z/MUL_ZZ_Z.h"
+#include "ADD_ZZ_Z/ADD_ZZ_Z.h"
+#include "LCM_NN_N/LCM_NN_N.h"
+
+NUMBQ addNumbQ(NUMBQ A, NUMBQ B)
+{
+	NUMBQ C;
+	if (A.a.A && A.b.A && B.a.A && B.b.A)
+	{
+		C.b = nokNumbN(A.b, B.b);
+		C.a = addNumbZ(multiplicationNumbZ(A.a, getNumbZ(B.b)), multiplicationNumbZ(B.a, getNumbZ(A.b)));
+	}
+	else
+		printf("Ошибка, числа не существует! \n");
+	return C;
+}
---
+++ DMKollok/DMKollok/Q/ADD_QQ_Q/LCM_NN_N/GCF_NN_N/MOD_NN_N/DIV_NN_N/SUB_NDN_N/SUB_NN_N/SUB_NN_N.c
@@ -0,0 +1,93 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "numbN.h"
+#include "bfunc/bfunc.h"
+#include "COM_NN_D/COM_NN_D.h"
+#include "SUB_NN_N.h"
+
+/*
+ 
+ Написал: Пискунов Виталий
+ Группа: 6308
+ 
+ differenceNUmb
+ 
+ Функция вычитание из одного натурального другое
+ 
+ Параметры:
+ 1) fNumb - Первое число
+ 2) sNumb - Второе число
+ 
+ Функция возращает тип - NUMBN
+ 
+ */
+
+
+
+NUMBN differenceNumb(NUMBN fNumb, NUMBN sNumb)
+    {
+        int error = 0;
+        NUMBN answer;
+        if(fNumb.A && sNumb.A)
+            {
+                int comp = compNumbers(fNumb, sNumb);
+                error = comp == -1;
+                if(!error)
+                    {
+                        if(comp == 1)
+                            swapNumb(&fNumb, &sNumb);
+                        
+                        answer.A = (int*) malloc (fNumb.n*sizeof(int));
+                        answer.n = fNumb.n;
+                        
+                        turnNumber(fNumb.A, fNumb.n);
+                        turnNumber(sNumb.A, sNumb.n);
+                        
+                        int min = 0;
+                        for(int i = 0; i < sNumb.n; ++i)
+                            {
+                                *(answer.A+i) = *(fNumb.A+i) - *(sNumb.A+i) - min;
+                                min = 0;
+                                if(*(answer.A+i) < 0)
+                                    {
+                                        *(answer.A+i) += 10;
+                                        min = 1;
+                                    }
+                                
+                            }
+                        if(sNumb.n!=fNumb.n)
+                            {
+                                for (int i = sNumb.n; i < fNumb.n; ++i)
+                                    {
+                                        *(answer.A+i) += *(fNumb.A+i) - min;
+                                        min = 0;
+                                    }
+                            }
+                        
+                        int fl = 0,
+                        i;
+                        
+                        for (i = fNumb.n-1; i >= 0 && !fl; --i)                         // Удаление нулей из числа
+                            fl = *(answer.A + i) != 0;
+                        
+                        i+=2;
+                        if(i != fNumb.n)
+                            {
+                                answer.n = i;
+                                answer.A = (int*) realloc(answer.A, i*sizeof(int));
+                            }
+
+                        turnNumber(answer.A, answer.n);
+                        turnNumber(fNumb.A, fNumb.n);
+                        turnNumber(sNumb.A, sNumb.n);
+                    }
+            }
+        else
+            error = 1;
+        
+        if(error)
+            printf("\nОшибка, одно из чисел не существует!\n");
+        
+        return answer;
+    }
